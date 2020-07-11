ef2a838ec3c9577a693ec4ea935d60e7b8b9b6c8
BG00924 <BG00924@users.noreply.github.com>
2020-07-11 15:54:01 -0500

    cont work on getuserrepos function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    //formats the github api to accept any user
+    var apiURL= "https://api.github.com/users/" + user + "/repos";
+    // make a request to the url
+    fetch(apiURL).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
 };
 
-getUserRepos();
+getUserRepos("microsoft");
